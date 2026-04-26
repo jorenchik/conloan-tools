dc793f4559089b79687c7a484da3e30c9292358a
jorenchik <jorens.stekels@gmail.com>
2026-04-26 12:57:47 +0300

    refactor: restructured the annotation CLI layout

--- src/conloan_tools/annotation/annotation.py
+++ src/conloan_tools/annotation/annotation.py
@@ -1,7 +1,6 @@
 import click
 
 from .make_sheet import make_sheet 
-from .inject_suggestions import inject_suggestions 
 from .translate import translate_target 
 from .validate_sheet import validate
 from .make_dataset import make_dataset 
@@ -10,8 +9,16 @@ from .make_dataset import make_dataset
 def annotation():
     """Conloan annotation utilities."""
 
-annotation.add_command(make_sheet)
-annotation.add_command(inject_suggestions)
-annotation.add_command(translate_target)
-annotation.add_command(validate)
-annotation.add_command(make_dataset)
+@annotation.group()
+def sheet():
+    """Conloan annotation sheet utilities."""
+
+sheet.add_command(make_sheet)
+sheet.add_command(validate)
+sheet.add_command(translate_target)
+
+@annotation.group()
+def json():
+    """Conloan dataset json utilities."""
+
+json.add_command(make_dataset)
